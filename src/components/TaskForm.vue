f35319cf8cfb7278598ca36b62692209c812888c
Trin4317 <eg.artourbabaev@gmail.com>
2023-03-04 13:11:53 +0000

    create task form component

---
+++ src/components/TaskForm.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+import { useTaskStore } from '../stores/TaskStore'
+
+const taskStore = useTaskStore()
+
+const newTask = ref('')
+
+const handleSubmit = () => {
+  if (newTask.value.length > 0) {
+    taskStore.addTask({
+      title: newTask.value,
+      isFav: false,
+      id: taskStore.totalCount + 1
+    })
+
+    newTask.value = ''
+  }
+}
+</script>
+
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="text" v-model="newTask" placeholder="I need to ..." />
+    <button>Add</button>
+  </form>
+</template>
