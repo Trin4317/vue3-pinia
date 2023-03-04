04752914814e74d5966400a5bf2d2ee7b7034d17
Trin4317 <eg.artourbabaev@gmail.com>
2023-03-04 11:32:27 +0000

    update default template

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
+  <main>
+    <header>
+      <img src="./assets/logo.svg" alt="pinia logo" />
+      <h1>Pinia Tasks</h1>
+    </header>
+  </main>
 </template>
