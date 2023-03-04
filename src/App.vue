011f32e80a94255c3fb063187e1c3db5d3daa5d1
Trin4317 <eg.artourbabaev@gmail.com>
2023-03-04 11:56:53 +0000

    import taskStore to App

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
-<script setup></script>
+<script setup>
+import { useTaskStore } from './stores/TaskStore'
+
+const taskStore = useTaskStore()
+</script>
 
 <template>
   <main>
     <header>
       <img src="./assets/logo.svg" alt="pinia logo" />
       <h1>Pinia Tasks</h1>
+      <h4>by {{ taskStore.name }}</h4>
     </header>
+
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <p>{{ task.title }}</p>
+      </div>
+    </div>
   </main>
 </template>
